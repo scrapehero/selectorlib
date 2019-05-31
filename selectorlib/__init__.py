f7cbe0213b85572427713c7b960017fa740ed661
Ashwin Rajeev <ashwin@scrapehero.com>
2019-05-31 12:15:28 +0530

    Bump version: 0.10.0 → 0.11.0

--- selectorlib/__init__.py
+++ selectorlib/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """scrapehero"""
 __email__ = 'pypi@scrapehero.com'
-__version__ = '0.10.0'
+__version__ = '0.11.0'
 
 from .selectorlib import Extractor  # noqa:F401
 from .formatter import Formatter  # noqa:F401
--- setup.py
+++ setup.py
@@ -51,6 +51,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/scrapehero/selectorlib',
-    version='0.10.0',
+    version='0.11.0',
     zip_safe=False,
 )
