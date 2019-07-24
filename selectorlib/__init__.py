30e914a301ae11d608236dbdc35a591a98083101
Ashwin Rajeev <ashwin@scrapehero.com>
2019-07-24 17:05:11 +0530

    Bump version: 0.11.0 → 0.12.0

--- selectorlib/__init__.py
+++ selectorlib/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """scrapehero"""
 __email__ = 'pypi@scrapehero.com'
-__version__ = '0.11.0'
+__version__ = '0.12.0'
 
 from .selectorlib import Extractor  # noqa:F401
 from .formatter import Formatter  # noqa:F401
