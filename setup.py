7987209f05f73a8d9ea55be06be948a5db03bd89
Ashwin Rajeev <ashwin@scrapehero.com>
2019-08-13 18:32:32 +0530

    Bump version: 0.13.0 → 0.14.0

--- setup.py
+++ setup.py
@@ -51,6 +51,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/scrapehero/selectorlib',
-    version='0.13.0',
+    version='0.14.0',
     zip_safe=False,
 )
